fb44f67c9f6052efb035b02e21288733cdb88582
Dav Karamyan <47416614+naymaraq@users.noreply.github.com>
2026-04-14 12:48:08 +0400

    Remove the PnC flag from the ASR inference pipelines (#15591)
    * remove enable PnC flag
    
    Signed-off-by: naymaraq <dkaramyan@nvidia.com>
    
    * bugfix
    
    Signed-off-by: naymaraq <dkaramyan@nvidia.com>
    
    * fix tutorial
    
    Signed-off-by: naymaraq <dkaramyan@nvidia.com>
    
    * rm pnc line from usage example
    
    Signed-off-by: naymaraq <dkaramyan@nvidia.com>
    
    * add functional tests for cache aware models
    
    Signed-off-by: naymaraq <dkaramyan@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: naymaraq <naymaraq@users.noreply.github.com>
    
    * rename augment_with_default -> fill_defaults
    
    Signed-off-by: naymaraq <dkaramyan@nvidia.com>
    
    * rm dead code
    
    Signed-off-by: naymaraq <dkaramyan@nvidia.com>
    
    * Apply isort and black reformatting
    
    Signed-off-by: naymaraq <naymaraq@users.noreply.github.com>
    
    * set enable pnc to false
    
    Signed-off-by: naymaraq <dkaramyan@nvidia.com>
    
    ---------
    
    Signed-off-by: naymaraq <dkaramyan@nvidia.com>
    Co-authored-by: naymaraq <dkaramyan@nvidia.com>

--- nemo/collections/asr/inference/pipelines/buffered_ctc_pipeline.py
+++ nemo/collections/asr/inference/pipelines/buffered_ctc_pipeline.py
@@ -163,9 +163,8 @@ class BufferedCTCPipeline(BasePipeline):
         """
         state = CTCStreamingState()
         state.set_global_offset(-self.initial_delay)
-        new_options = options.augment_with_defaults(
-            default_enable_itn=self.text_processor.is_itn_enabled(),
-            default_enable_pnc=self.text_processor.is_pnc_enabled(),
+        new_options = options.fill_defaults(
+            default_enable_itn=self.text_processor.itn_enabled,
             default_enable_nmt=self.nmt_enabled,
             default_source_language=self.nmt_model.source_language if self.nmt_enabled else None,
             default_target_language=self.nmt_model.target_language if self.nmt_enabled else None,
