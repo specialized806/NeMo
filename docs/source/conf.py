f3a7ebc4bce7bc5789208195a5e92624d25abc55
Piotr Żelasko <pzelasko@nvidia.com>
2026-08-10 09:41:09 -0400

    Update SpeechLM2 for Automodel main (#16021)
    * Update SpeechLM2 for Automodel main
    
    * Fix H100plus Automodel runtime dependencies
    
    * Fix SpeechLM2 HF export with Automodel

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,6 @@ autodoc_mock_imports = [
     'torch.utils.data',
     'torch.utils.data.sampler',
     'torchtext',
-    'torchvision',
     'hydra',  # hydra-core in requirements, hydra during import
     'transformers.tokenization_bert',  # has ., troublesome for this regex
     'sklearn',  # scikit_learn in requirements, sklearn in import
