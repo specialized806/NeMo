1f73a910bc0f5ab774d100706329730b81457591
Piotr Żelasko <pzelasko@nvidia.com>
2026-09-03 12:16:09 -0400

    Update licenses and add third party notice (#16128)
    * Add third-party notices
    
    Signed-off-by: Piotr Żelasko <pzelasko@nvidia.com>
    
    * Update source license headers
    
    Signed-off-by: Piotr Żelasko <pzelasko@nvidia.com>
    
    * Add NVIDIA copyright to LICENSE
    
    Signed-off-by: Piotr Żelasko <pzelasko@nvidia.com>
    
    * Document DCO sign-off requirements
    
    Signed-off-by: Piotr Żelasko <pzelasko@nvidia.com>
    
    * Fix BSD-2-Clause SPDX identifier
    
    Signed-off-by: Piotr Żelasko <pzelasko@nvidia.com>
    
    * Complete inline third-party attributions
    
    Signed-off-by: Piotr Żelasko <pzelasko@nvidia.com>
    
    * Fix lint failures exposed by license header update
    
    Signed-off-by: Piotr Żelasko <pzelasko@nvidia.com>
    
    ---------
    
    Signed-off-by: Piotr Żelasko <pzelasko@nvidia.com>

--- nemo/collections/tts/parts/utils/tts_dataset_utils.py
+++ nemo/collections/tts/parts/utils/tts_dataset_utils.py
@@ -1,4 +1,5 @@
-# Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES.  All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# BetaBinomialInterpolator is adapted from NVIDIA Deep Learning Examples:
+# Copyright (c) 2019-present, NVIDIA CORPORATION. Licensed under the BSD-3-Clause License.
+# Source: https://github.com/NVIDIA/DeepLearningExamples
 
 import functools
 import importlib
